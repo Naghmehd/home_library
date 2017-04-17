db4c7406a2bc9c0cb910b3be856ae64489ac4424
Naghmeh Shirazi <naghmehd@gmail.com>
2017-04-16 22:13:39 -0500

    added angular2 token

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Angular2TokenService } from 'angular2-token';
+
 import { HomeLibraryRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -19,7 +21,7 @@ import { NewUserComponent } from './new-user/new-user.component';
     HttpModule,
     HomeLibraryRoutingModule
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
